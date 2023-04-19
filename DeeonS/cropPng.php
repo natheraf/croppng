d12476c5e5999e1a389ddcbecbfe198e0767b102
DeeonStjean <deeonstjean101@gmail.com>
2023-04-18 22:43:08 -0400

    <?php require_once(__DIR__ . '../../vendor/autoload.php');
    use Cloudinary\Cloudinary;
    // Use the Configuration class
    use Cloudinary\Configuration\Configuration;
    // Use the UploadApi class for uploading assets
    use Cloudinary\Api\Upload\UploadApi;
    // Use the Resize transformation group and the ImageTag class
    use Cloudinary\Transformation\Resize;
    use Cloudinary\Transformation\Background;
    use Cloudinary\Transformation\Gravity;
    use Cloudinary\Tag\ImageTag;
    
    $cloudinary = new Cloudinary(
        [
            'cloud' => [
                'cloud_name' => 'durd8kb5e',
                'api_key'    => '215961418216461',
                'api_secret' => '6AOiayvtgYxKvBUsru5R6Hqw1_U',
            ],
        ]
    );
    Configuration::instance('cloudinary://215961418216461:6AOiayvtgYxKvBUsru5R6Hqw1_U@durd8kb5e?secure=true');
    
    $result = $cloudinary->uploadApi()->upload($imageURL, [
        "folder" => "CropPNG/imgs"
    ]);
    
    // Create the image tag with the transformed image
    $imgtag = (new ImageTag($result['public_id']))
    ->resize(Resize::fill()->width($croppedWidth)
    ->height($croppedHeight)
    ->gravity(
    Gravity::autoGravity())
    );
    
    ?>

---
+++ DeeonS/cropPng.php
@@ -0,0 +1,46 @@
+<?php
+$uploadType = $_POST['upload']; // singleUpload zipUpload
+$apiType = $_POST['apiType']; // cloudinaryAPI imaggaAPI
+$croppedWidth = $_POST['croppedWidth'];
+$croppedHeight = $_POST['croppedHeight'];
+
+if ($croppedWidth == '') {
+    $croppedWidth = 500;
+}
+if ($croppedHeight == '') {
+    $croppedHeight = 500;
+}
+if($uploadType == "upload"){
+    echo 'Type of upload recieved: Single Image upload';
+
+    if($uploadType == ''){
+        echo 'No Image found';
+        if ($_FILES['fileUpload']['error'][0] !== UPLOAD_ERR_OK) {
+            die('Error: No file uploaded or upload failed');
+        }
+        echo 'Uploaded File found! <br>Checking if file is image...<br>';
+
+        $info = getimagesize($_FILES['fileUpload']['tmp_name'][0]);
+        if ($info === FALSE) {
+            die("Unable to determine image type of uploaded file");
+        }
+         echo 'Image type found! <br>Checking if image is JPEG or PNG...<br>';
+         if (($info[2] !== IMAGETYPE_JPEG) && ($info[2] !== IMAGETYPE_PNG)) {
+            die("Not a JPEG/PNG");
+        }else {
+        echo 'Image URL found!<br>';
+
+        echo '<h3>Original Image<h3><br>';
+        echo '<img src="' . $uploadType . '" alt="original_image"><br>';
+        echo '<h3>Cropped Image<h3><br>';
+        if ($apiType == "cloudinaryAPI") {
+            include "./cloudinaryAPI.php";
+            echo $imgtag;
+        } else {
+            include "./imaggaAPI.php";
+        }
+    }
+}else {
+    echo 'upload type not found';
+}
+?>
